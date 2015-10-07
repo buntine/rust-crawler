a00af8477164a6443e46dbbad80f7522f16d90b0
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-10-07 23:11:04 +1100

    Setup private helper fn

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-struct Crawler {
-    base: &'static str,
-    visited: Vec<String>,
-}
-
-impl Crawler {
-    fn new(base: &'static str) -> Crawler {
-        Crawler{base: base, visited: vec![]}
+mod crawl {
+    pub struct Crawler {
+        pub base: String,
+        visited: Vec<String>,
     }
 
-    fn crawl(&mut self) {
-        println!("Crawling {};", self.base);
+    impl Crawler {
+        pub fn new(base: String) -> Crawler {
+            Crawler{base: base, visited: vec![]}
+        }
+
+        pub fn crawl(&mut self) {
+            let base = self.base.clone();
+            self.parseWebpage(0, &base)
+        }
+
+        fn parseWebpage(&mut self, depth: i32, url: &String) {
+            println!("Parsing {}", url);
+        }
     }
 }
 
 fn main() {
-    let mut crawler = Crawler::new("http://cloudopenweek.deakin.edu.au/");
+    let mut crawler = crawl::Crawler::new("http://cloudopenweek.deakin.edu.au/".to_string());
 
     crawler.crawl();
 }
