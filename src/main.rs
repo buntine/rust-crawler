3c4b5621edbea9f80c89ca727d9da0f6a80b386c
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-10-07 22:20:19 +1100

    Started struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Crawler {
+    base: &'static str,
+    visited: Vec<String>,
+}
+
+impl Crawler {
+    fn new(base: &'static str) -> Crawler {
+        Crawler{base: base, visited: vec![]}
+    }
+
+    fn crawl(&mut self) {
+        println!("Crawling {};", self.base);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut crawler = Crawler::new("http://cloudopenweek.deakin.edu.au/");
+
+    crawler.crawl();
 }
